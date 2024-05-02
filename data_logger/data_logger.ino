6f5212db2d668a634e6a54f8711987b768311d01
iquesambi <hfreitas@kth.se>
2024-05-02 15:38:11 +0200

    data logger working for all sensors + rotary

--- data_logger/data_logger.ino
+++ data_logger/data_logger.ino
@@ -1,30 +1,22 @@
-/*
-  SD card datalogger
-
-  This example shows how to log data from three analog sensors
-  to an SD card using the SD library.
-
-  The circuit:
-   analog sensors on analog ins 0, 1, and 2
-   SD card attached to SPI bus as follows:
- ** MOSI - pin 11
- ** MISO - pin 12
- ** CLK - pin 13
- ** CS - pin 4 (for MKRZero SD: SDCARD_SS_PIN)
-
-  created  24 Nov 2010
-  modified 9 Apr 2012
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-*/
 
 #include <SPI.h>
 #include <SD.h>
 #include <Wire.h>
 #include <MPU6050.h>
+
+#include "Button2.h"; //  https://github.com/LennartHennigs/Button2
+#include "Rotary.h";
+
+#define ROTARY_PIN1	5
+#define ROTARY_PIN2	6
+#define BUTTON_PIN	7
+
+#define CLICKS_PER_STEP   4   // this number depends on your rotary encoder 
+
+
 MPU6050 mpu;
+Rotary r;
+Button2 b;
 
 const int chipSelect =10;
 
@@ -37,7 +29,14 @@ void setup() {
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
+r.begin(ROTARY_PIN1, ROTARY_PIN2, CLICKS_PER_STEP);
+  r.setChangedHandler(rotate);
+  r.setLeftRotationHandler(showDirection);
+  r.setRightRotationHandler(showDirection);
 
+  b.begin(BUTTON_PIN);
+  b.setTapHandler(click);
+  b.setLongClickHandler(resetPosition);
 
   Serial.print("Initializing SD card...");
 
@@ -48,10 +47,36 @@ void setup() {
     while (1);
   }
   Serial.println("card initialized.");
+  File dataFile = SD.open("datalog.csv", FILE_WRITE);
+
+  // if the file is available, write to it:
+  if (dataFile) {
+    dataFile.print("tiltX");
+    dataFile.print(",");
+    dataFile.print("tiltY");
+    dataFile.print(",");
+    dataFile.print("tiltZ");
+     dataFile.print(",");
+    dataFile.print("accAngleX");
+    dataFile.print(",");
+    dataFile.print("accAngleY");
+    dataFile.print(",");
+    dataFile.print("accAngleZ");
+    dataFile.print(",");
+    dataFile.print("gyroX");
+    dataFile.print(",");
+    dataFile.print("gyroY");
+    dataFile.print(",");
+    dataFile.println("gyroZ");
+    dataFile.close();
+  }
 }
 
 void loop() {
 
+ r.loop();
+  b.loop();
+  
     // Read accelerometer data
   int16_t accX, accY, accZ;
   mpu.getAcceleration(&accX, &accY, &accZ);
@@ -90,11 +115,18 @@ void loop() {
     dataFile.print(tiltY);
     dataFile.print(",");
     dataFile.print(tiltZ);
+     dataFile.print(",");
     dataFile.print(accAngleX);
     dataFile.print(",");
     dataFile.print(accAngleY);
     dataFile.print(",");
-    dataFile.println(accAngleZ);
+    dataFile.print(accAngleZ);
+    dataFile.print(",");
+    dataFile.print(gyroX);
+    dataFile.print(",");
+    dataFile.print(gyroY);
+    dataFile.print(",");
+    dataFile.println(gyroZ);
     dataFile.close();
     // print to the serial port too:
     Serial.print(tiltX);
@@ -102,13 +134,132 @@ void loop() {
     Serial.print(tiltZ);
     Serial.print(accAngleX);
     Serial.print(accAngleY);
-    Serial.println(accAngleZ);
+    Serial.print(accAngleZ);
+    Serial.print(gyroX);
+    Serial.print(gyroY);
+    Serial.println(gyroZ);
 
   }
   // if the file isn't open, pop up an error:
   else {
-    Serial.println("error opening datalog.txt");
+    Serial.println("error opening datalog.csv");
   }
     }
   }
+}
+
+void rotate(Rotary& r) {
+   Serial.println(r.getPosition());
+    
+     File dataFile = SD.open("datalog.csv", FILE_WRITE);
+  if (dataFile) {
+    dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.println(r.getPosition());
+    dataFile.close();
+}
+
+}
+
+// on left or right rotation
+void showDirection(Rotary& r) {
+  Serial.println(r.directionToString(r.getDirection()));
+
+       File dataFile = SD.open("datalog.csv", FILE_WRITE);
+  if (dataFile) {
+     dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.println(r.directionToString(r.getDirection()));
+    dataFile.close();
+}
+}
+ 
+// single click
+void click(Button2& btn) {
+  Serial.println("Click!");
+        File dataFile = SD.open("datalog.csv", FILE_WRITE);
+  if (dataFile) {
+     dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.println("Click!");
+    dataFile.close();
+}
+}
+
+// long click
+void resetPosition(Button2& btn) {
+  r.resetPosition();
+  Serial.println("Reset!");
+
+          File dataFile = SD.open("datalog.csv", FILE_WRITE);
+  if (dataFile) {
+     dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.print(" ");
+    dataFile.print(",");
+    dataFile.println("Long click");
+    dataFile.close();
+}
 }
