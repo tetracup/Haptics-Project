617079bfba36fb54585aec1a38748537b167e591
iquesambi <hfreitas@kth.se>
2024-05-30 13:50:28 +0200

    BT works, gesture works

--- newBLE/newBLE.ino
+++ newBLE/newBLE.ino
@@ -1,12 +1,3 @@
-///
-///   ble_shining_kb.ino
-///   
-///   created: 2020-11
-///
-///  Simulate an evil writing machine in an isolated hotel on winter. 
-///  No hardware required other than an Nano33 ble.
-///
-
 #include "Nano33BleHID.h"
 #include "signal_utils.h"
 
@@ -14,65 +5,31 @@
 
 Nano33BleKeyboard bleKb("Music ball");
 
-// Tracking index for the end of the writing animation ticker.
-int sTickerIndex = -1;
 
-static const char kSentence[] = "      ";
-const char letter = 'K'; 
-static const int kNumTickerSteps = 4;
-
-// How long it takes for the sentence to be written.
-static const int kSentenceDurationMilliseconds = 4029;
-
-// How long it takes before the sentence is rewritten.
-static const int kSentenceDelayMilliseconds = 1977;
-
-// How long one writing animation will run.
-static const int kSentenceTotalTimeMilliseconds = kSentenceDurationMilliseconds + kSentenceDelayMilliseconds;
-
+char letter = ' '; 
 // Safeguard to terminate this mess of an app before going crazy.
-static const int kTotalRuntime = 8 * kSentenceTotalTimeMilliseconds;
 
-
-/* -------------------------------------------------------------------------- */
-
-int before; 
 void setup()
 {
-  
-  // General setup.
-  pinMode(LED_BUILTIN, OUTPUT);
-
   // Initialize both BLE and the HID.
   bleKb.initialize();
 
   // Launch the event queue that will manage both BLE events and the loop. 
   // After this call the main thread will be halted.
   MbedBleHID_RunEventThread();
-  before = millis(); 
+
 }
 
-void loop()
-{
+void loop(){
   // When disconnected, we animate the builtin LED to indicate the device state.
   if (bleKb.connected() == false) {
-    return;
-  }
-
-  // Stop when we reach a certain runtime (better safe than sorry).
-  if (bleKb.connection_time() > kTotalRuntime) {
     return;
   }
 
   // Retrieve the HIDService to update.
   auto *kb = bleKb.hid();
 
-  // The animation is divided in two parts :
-  if (before - millis() >= 3000)
-  {
     // Write the sentence using the StringWriter object.
-    &kb.sendCharacter(letter);
-  }
+    kb->sendCharacter(letter);
+  
 }
-
-/* -------------------------------------------------------------------------- */
